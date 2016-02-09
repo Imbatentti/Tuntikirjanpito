c6e34415cc1c3b67a2e2ae3d9150606b1834d3b7
janitapaninen <jani.tapaninen@gmail.com>
2016-02-09 11:14:31 +0200

    paskaa persdeestä

--- tunnit/src/main/java/dao/KayttajaDAO.java
+++ tunnit/src/main/java/dao/KayttajaDAO.java
@@ -8,21 +8,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
+import bean.SyoteVali;
 
 public class KayttajaDAO {
 	static Connection yhteys = null;
 
-	public static Connection avaaYhteys() throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, SQLException {
-		
-		Class.forName(DBConnectionProperties.getInstance().getProperty("driver")).newInstance();
-		//haetaan tiedot .properties tiedostosta
-		yhteys = DriverManager.getConnection(
-				DBConnectionProperties.getInstance().getProperty("url"), 
-				DBConnectionProperties.getInstance().getProperty("username"),
-				DBConnectionProperties.getInstance().getProperty("password"));
-		
+	public static Connection avaaYhteys() throws InstantiationException,
+			IllegalAccessException, ClassNotFoundException, IOException,
+			SQLException {
+
+		Class.forName(
+				DBConnectionProperties.getInstance().getProperty("driver"))
+				.newInstance();
+		// haetaan tiedot .properties tiedostosta
+		yhteys = DriverManager.getConnection(DBConnectionProperties
+				.getInstance().getProperty("url"), DBConnectionProperties
+				.getInstance().getProperty("username"), DBConnectionProperties
+				.getInstance().getProperty("password"));
 
 		try {
 			// YHTEYDEN AVAUS
@@ -47,7 +49,38 @@ public class KayttajaDAO {
 					.println("Tietokantayhteys ei jostain syyst‰ suostu menem‰‰n kiinni.");
 		}
 	}
-	
-	
+
+	public void lisaa(SyoteVali syotettavatTunnit)
+			throws InstantiationException, IllegalAccessException,
+			ClassNotFoundException, IOException, SQLException {
+
+		String ktunnus = syotettavatTunnit.getNimi();
+		double tunnit = syotettavatTunnit.getTunnit();
+		String kuvaus = syotettavatTunnit.getKuvausVali();
+		int id;
+		
+		try {
+			Connection yhteys = avaaYhteys();
+
+			String sql1 = "select kayttaja_id from KAYTTAJAT where kayttajatunnus =(?);";
+			PreparedStatement lause1 = yhteys.prepareStatement(sql1);
+			lause1.setString(1, ktunnus);
+			ResultSet rs = lause1.executeQuery();
+			id = rs.getInt("kayttaja_id");
+				
+			
+			String sql2 = "insert into TUNNIT (tuntien_maara, kuvaus, kayttaja_id) values(?,?,?);";
+			PreparedStatement lause2 = yhteys.prepareStatement(sql2);
+			lause2.setDouble(1, tunnit);
+			lause2.setString(2, kuvaus);
+			lause2.setInt(3, id);
+			lause2.executeUpdate();
+
+		} 
+		finally {
+			suljeYhteys();
+		}
+
+	}
 
 }
