4e0140ec3172db5df6119782cf17f99a12fad5db
Imbatentti <ru334@hotmail.com>
2016-02-09 09:57:37 +0200

    kayttajaidt

--- tunnit/src/main/java/bean/tunnit.java
+++ tunnit/src/main/java/bean/tunnit.java
@@ -4,15 +4,19 @@ public class Tunnit {
 
 	private double tuntimaara;
 	private String kuvaus;
+	private String tunnitKayttajaId;
 
-	public Tunnit(double tuntimaara, String kuvaus) {
+	public Tunnit(double tuntimaara, String kuvaus, String tunnitKayttajaId) {
 		this.tuntimaara = tuntimaara;
 		this.kuvaus = kuvaus;
+		this.tunnitKayttajaId = tunnitKayttajaId;
 	}
 
 	public Tunnit() {
 		tuntimaara = 0;
 		kuvaus = null;
+		tunnitKayttajaId = null;
+		
 	}
 
 	public double getTuntimaara() {
@@ -29,6 +33,14 @@ public class Tunnit {
 
 	public void setKuvaus(String kuvaus) {
 		this.kuvaus = kuvaus;
+	}
+
+	public String getTunnitKayttajaId() {
+		return tunnitKayttajaId;
+	}
+
+	public void setTunnitKayttajaId(String tunnitKayttajaId) {
+		this.tunnitKayttajaId = tunnitKayttajaId;
 	}
 
 }
