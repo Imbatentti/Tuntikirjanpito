aa74f76b25c0c1e85363ebcff43c29042800e9ce
janitapaninen <jani.tapaninen@gmail.com>
2016-03-08 08:57:58 +0200

    spring web dependency

--- tunnit/src/main/java/servlet/KayttajaController.java
+++ tunnit/src/main/java/servlet/KayttajaController.java
@@ -7,10 +7,10 @@ import org.springframework.ui.Model;
 
 import bean.Tunnit;
 import dao.KayttajaDAO;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
