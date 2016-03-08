bf3f5455c4edf358fa70e98211b996fe9df5187a
janitapaninen <jani.tapaninen@gmail.com>
2016-03-08 09:34:04 +0200

    webapp rakenne korjattu

--- tunnit/src/main/java/servlet/KayttajaController.java
+++ tunnit/src/main/java/servlet/KayttajaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-//@RequestMapping (value="/tunnit")
+@RequestMapping (value="/tunnit")
 public class KayttajaController {
 	
 	@Inject
@@ -29,12 +29,12 @@ public class KayttajaController {
 	}
 	
 	//FORMIN TEKEMINEN
-	//@RequestMapping(value="uusitunti", method=RequestMethod.GET)
-	public String getCreateForm(Model model){
+	@RequestMapping(value="uusitunti", method=RequestMethod.GET)
+	public String getLomake(Model model){
 		Tunnit uudetTunnit =  new Tunnit();
 		
 		model.addAttribute("tunnit", uudetTunnit);
-		return "tunnit/createForm";
+		return "tunnit/lomake";
 	}
 	
 	//FORMIN VASTAANOTTO
