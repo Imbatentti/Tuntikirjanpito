330ede57d76265d8fd506f9ee9e0c4fbc5fe1be4
Imbatentti <ru334@hotmail.com>
2016-04-28 11:19:55 +0300

    POST

--- tunnit/src/main/java/fi/softala/tunnit/controller/TulostusController.java
+++ tunnit/src/main/java/fi/softala/tunnit/controller/TulostusController.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,15 +35,23 @@ public class TulostusController {
 	//KÄYTTÄJÄN TUNTIEN TULOSTUS
 	@RequestMapping(value="kayttajaTulostus", method=RequestMethod.GET)
 	public String getKayttajatulostus(Model model) {
-		List<Projektisumma> kayttajatunnit = dao.haeKayttajaSumma();
-		List<Tulostus> tulostus = dao.haeKayttajanTunnit();
+		//List<Projektisumma> kayttajatunnit = dao.haeKayttajaSumma();
+		//List<Tulostus> tulostus = dao.haeKayttajanTunnit();
 		List<KayttajaTulostus> kayttajatulostus = dao.haeKayttajat();
 		model.addAttribute("kayttajatulostus", kayttajatulostus);
-		model.addAttribute("kayttajatunnit", kayttajatunnit);
-		model.addAttribute("tulostus", tulostus);
+		//model.addAttribute("kayttajatunnit", kayttajatunnit);
+		//model.addAttribute("tulostus", tulostus);
 
 		return "/sisalto/kayttajaTulostus";
 	}
+	
+	//KÄYTTÄJÄN VALITSEMINEN
+	@RequestMapping(value="tulostus", method=RequestMethod.POST)
+	public String kayttajat ( @ModelAttribute(value="kayttaja") KayttajaTulostus kayttajatulostus){
+		dao.haeKayttajanTunnit(kayttajatulostus);
+		return "/sisalto/kayttajaTulostus";
+	}
+	
 	
 	// KAIKKIEN TUNTIEN TULOSTUS
 	@RequestMapping(value="tulostus", method=RequestMethod.GET)
